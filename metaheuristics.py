4d8aab38eca8c2c33b3dc2346bdcf6c7ec4bf117
dreivmeister <felix.brandt@fh-bielefeld.de>
2023-06-27 12:59:59 +0200

    es and ga

--- metaheuristics.py
+++ metaheuristics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # for now: S is a np.ndarray of dim=1 and fixed length with real entries
 
-
+# SINGLE STATE METHODS
 def hill_climbing(S, Tweak, Copy, Quality, optimal, maxiter=100):
     # S - initial candidate solution
     # Tweak - problem specific function, 
@@ -238,7 +238,158 @@ def tabu_search(l, n, Generate, Tweak, Copy, Quality, optimal, maxiter=100):
                     return Best
     return Best
                 
+
+def iterated_local_search_w_random_restarts(T, Home, Perturb, Generate, Tweak, Copy, Quality, optimal, maxiter=100):
+    # T - distribution of possible time intervals
+    # Home - function, determines wether to adopt new home base
+    # Tweak - problem specific function, 
+    # takes candidate solution, returns randomly slightly different solution
+    # Copy - problem specific function, 
+    # takes candidate solution, returns a copy
+    # Quality - problem specific function,
+    # takes candidate solution, returns quality of it (goodness)
+    # optimal - threshold of optimal solution
+    
+    S = Generate()
+    Best = S
+    H = S # home base
+    Best = S
+    QB = Quality(Best)
+    for _ in range(maxiter):
+        time = random_sample(T)
+        for i in range(time):
+            R = Tweak(Copy(S))
+            QS = Quality(S)
+            if Quality(R) > QS:
+                S = R
+                if QS >= optimal:
+                    return S
+                
+        QS = Quality(S)
+        if QS > QB:
+            Best = S
+            QB = QS
+            if QB >= optimal:
+                return Best
+            
+        H = Home(H, S) # tricky to implement exploration vs. exploitation
+        S = Perturb(H)
+    return Best
+
+
+
+# POPULATION METHODS
+def abstract_generational_ea(InitialPopulation, AssesFitness, Fitness, Breed, Join, optimal, maxiter):
+    P = InitialPopulation()
+    Best = None
+    for _ in range(maxiter):
+        P_fitness = AssesFitness(P) # fitness for each individual
+        for i in range(len(P)):
+            if Best is None or P_fitness[i] > Fitness(Best):
+                Best = P[i]
+                if Fitness(Best) >= optimal:
+                    return Best
+        P = Join(P, Breed(P))
+    return Best
+
+
+
+def mu_lambda_evolution_strategy(mu, lam, Choose_mu, InitialPopulation, AssesFitness, Fitness, Copy, Mutate, optimal, maxiter):
+    # mu - number of parents selected
+    # lam - number of children generated
+    
+    P = InitialPopulation(lam)
+    Best = None
+    for _ in range(maxiter):
+        P_fitness = AssesFitness(P) # fitness for each individual
+        for i in range(len(P)):
+            if Best is None or P_fitness[i] > Fitness(Best):
+                Best = P[i]
+                if Fitness(Best) >= optimal:
+                    return Best
+        
+        Q = Choose_mu(mu, P, P_fitness)
+        P = []
+        for i in range(len(Q)):
+            for _ in range(int(lam/mu)):
+                P.append(Mutate(Copy(Q[i])))
+    return Best
+    
+def mu_plus_lambda_evolution_strategy(mu, lam, Choose_mu, InitialPopulation, AssesFitness, Fitness, Copy, Mutate, optimal, maxiter):
+    # mu - number of parents selected
+    # lam - number of children generated
+    
+    P = InitialPopulation(lam)
+    Best = None
+    for _ in range(maxiter):
+        P_fitness = AssesFitness(P) # fitness for each individual
+        for i in range(len(P)):
+            if Best is None or P_fitness[i] > Fitness(Best):
+                Best = P[i]
+                if Fitness(Best) >= optimal:
+                    return Best
+        
+        Q = Choose_mu(mu, P, P_fitness) # choose mu best individuals
+        P = Q
+        for i in range(len(Q)):
+            for _ in range(int(lam/mu)):
+                P.append(Mutate(Copy(Q[i])))
+    return Best
+    
+    
+def genetic_algorithm(popsize, InitialPopulation, AssesFitness, Fitness, SelectWithReplacement, Crossover, Copy, Mutate, optimal, maxiter):
+    
+    P = InitialPopulation(popsize)
+    Best = None
+    for _ in range(maxiter):
+        P_fitness = AssesFitness(P) # fitness for each individual
+        for i in range(len(P)):
+            if Best is None or P_fitness[i] > Fitness(Best):
+                Best = P[i]
+                if Fitness(Best) >= optimal:
+                    return Best
+                
+        Q = []
+        for _ in range(int(popsize/2)):
+            Pa = SelectWithReplacement(P)
+            Pb = SelectWithReplacement(P)
+            Ca, Cb = Crossover(Copy(Pa), Copy(Pb))
+            Q.append(Mutate(Ca))
+            Q.append(Mutate(Cb))
+        P = Q
+    return Best
             
     
-    
+def random_boolean_vector(l):
+    return np.random.choice(a=[False, True], size=l, p=[0.5, 0.5])
+
+def bit_flip_mutation(p, v):
+    for i in range(len(v)):
+        if p >= np.random.uniform():
+            v[i] = not v[i]
+    return v
+
+def one_point_crossover(v, w):
+    c = np.random.randint(1, len(v))
+    if c != 1:
+        for i in range(c, len(v)):
+            v[i],w[i] = w[i],v[i]
+    return v, w
+
+def two_point_crossover(v, w):
+    c = np.random.randint(1, len(v))
+    d = np.random.randint(1, len(v))
+    if c > d:
+        c, d = d, c
+    if c != d:
+        for i in range(c, d):
+            v[i],w[i] = w[i],v[i]
+    return v, w
+
+def uniform_crossover(p, v, w):
+    for i in range(len(v)):
+        if p >= np.random.uniform():
+            v[i],w[i] = w[i],v[i]
+    return v, w
+            
     
